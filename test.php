4c000efb6d4754fca401a96cd699577943ca56a5
HardCarroll <phanchan_hc@163.com>
2020-05-29 19:15:03 +0800

    20200529

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-$f = $_FILES["files"];
-foreach($f["tmp_name"] as $key=>$val) {
-  move_uploaded_file($val, $_SERVER["DOCUMENT_ROOT"]."/upload/".$f["name"][$key]);
-}
-// echo json_encode($res, 320);
